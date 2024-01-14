49cd1a72437f54ac3eb4eda226fda17c59af3ba2
Chaima <chaima.akremi.1997@gmail.com>
2024-01-14 11:46:27 +0100

    [EVOL] - Display services list

--- src/app/services-list/services-list.component.ts
+++ src/app/services-list/services-list.component.ts
@@ -1,15 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { CommonModule } from '@angular/common';
+import { ServiceInterface } from '../models/service.interface';
+import { ServiceService } from '../api-services/service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-services-list',
   standalone: true,
     imports: [
-      HeaderComponent
+      HeaderComponent,
+      CommonModule
     ],
   templateUrl: './services-list.component.html',
   styleUrl: './services-list.component.css'
 })
-export class ServicesListComponent {
+export class ServicesListComponent implements OnInit {
 
+  services: Array<ServiceInterface>;
+
+  constructor(
+    private serviceService: ServiceService,
+    private router: Router,
+  ) {
+  }
+
+  ngOnInit() {
+    this.services = [];
+    this.serviceService.getList().subscribe(
+      response => {
+        this.services = response;
+      }
+    );
+  }
+
+  navigateTo(page: string) {
+    this.router.navigate([page]);
+  }
 }
