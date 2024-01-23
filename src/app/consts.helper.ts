d3811549919d907fa39a5c03ff5674102493a2da
Chaima <chaima.akremi.1997@gmail.com>
2024-01-23 20:40:09 +0100

    [EVOL] - Check access page by user role

--- src/app/consts.helper.ts
+++ src/app/consts.helper.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ConstsHelper {
+  static readonly ROLE_ADMINISTRATOR = 'administrator';
+  static readonly ROLE_EMPLOYEE = 'employee';
   static readonly ERROR_OCCURRED_RETRY_MESSAGE = 'Oups, nous avons rencontré un petit souci... Nous vous invitons à ré-essayer plus tard.';
   static readonly emailPattern: RegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   static readonly phoneNumber: RegExp = /^0[1|2|3|4|5|6|7][0-9]{8}$/;
